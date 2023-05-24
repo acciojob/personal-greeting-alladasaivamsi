d393b5b74b0dba85f85edc1c9349b6e79c666371
alladasaivamsi <alladasaivamsi@gmail.com>
2023-05-24 17:24:24 +0530

    submitting code-Wed May 24 2023 17:24:23 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 
-import React from "react";
+import React , {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  function greeting(event) {
+    event.preventDefault();
+    let greet = event.terget.value;
+    if(greet != "")
+    {
+      document.getElementById('greeting').innerHTML = `Hello ${greet}`;
+    }
+    else
+    {
+      document.getElementById('greeting').innerHTML = "";
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form>
+        <label for="name">Enter your name:</label>
+        <br/>
+        <input id="name" type="text" onInput={{greeting}}/>
+      </form>
+      <p id="greeting"></p>
     </div>
   )
 }
 
-export default App
+export default App;
